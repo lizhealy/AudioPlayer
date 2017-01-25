d083ee03d1c6b559d86805efc009b809f059b6e4
lizhealy <lizhealy8@gmail.com>
2017-01-25 16:32:23 -0500

    Initial Commit

--- Audio Player/MaggieViewController.swift
+++ Audio Player/MaggieViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MaggieViewController.swift
 //  Audio Player
 //
 //  Created by Liz Healy on 1/25/17.
@@ -9,51 +9,46 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var durationSlider: UISlider!
+class MaggieViewController: UIViewController {
     
     var player = AVAudioPlayer()
     var songTime = Timer()
-    let audioPath = Bundle.main.path(forResource: "rehab", ofType: "mp3")
+    let audioPath = Bundle.main.path(forResource: "alaska", ofType: "mp3")
     
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBOutlet weak var durationSlider: UISlider!
+    
+    @IBAction func play(_ sender: Any) {
+        player.play()
+        
+        songTime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(MaggieViewController.updateTime), userInfo: nil, repeats: true)
+    }
+    
+    func updateTime() {
+        durationSlider.value = Float(player.currentTime)
+    }
 
-    
     @IBAction func pause(_ sender: Any) {
         player.pause()
         songTime.invalidate()
     }
     
-    @IBAction func play(_ sender: Any) {
-        
-        player.play()
-        
-        
-        songTime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
-
-    
-    }
-    
-    @IBAction func back(_ sender: Any) {
-        songTime.invalidate()
-        player.stop()
-        durationSlider.value = 0
-    }
-    @IBAction func sliderMoved(_ sender: Any) {
-        
-        player.volume = slider.value
+    @IBAction func volumeSliderMoved(_ sender: Any) {
+        player.volume = volumeSlider.value
     }
     
     @IBAction func durationSliderMoved(_ sender: Any) {
         player.currentTime = TimeInterval(durationSlider.value)
     }
     
-    
-    @IBOutlet weak var pause: UIButton!
+    @IBAction func back(_ sender: Any) {
+        player.stop()
+        songTime.invalidate()
+        durationSlider.value = 0
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         
         
@@ -62,24 +57,17 @@ class ViewController: UIViewController {
             durationSlider.maximumValue = Float(player.duration)
             
             
-            
         }
         catch {
             //process errors
         }
-        
-    }
 
-    
-    func updateTime() {
-        durationSlider.value = Float(player.currentTime)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
 }
-
